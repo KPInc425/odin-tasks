95d651622967e3b1a2c6114b79f87c58bd98d755
KP <kpinc425@gmail.com>
2022-05-17 13:04:57 -0700

    Implement drop down menu

---
+++ src/displayTaskMainMenu.js
@@ -0,0 +1,38 @@
+function displayTaskMainMenu(displayContainer) {
+    const dropDownContainer = document.createElement('div');
+    dropDownContainer.classList.add('dropDown');
+
+    const dropDownButton = document.createElement('button');
+    dropDownButton.classList.add('dropDownButton');
+    dropDownButton.innerText = "Task Menu";
+
+    const dropItemsContainer = document.createElement('div');
+    dropItemsContainer.id = "dropDownItemsContainer";
+    dropItemsContainer.classList.add('dropDownContent');
+
+    const menuItem1 = document.createElement('a');
+    menuItem1.setAttribute('href', "#")
+    menuItem1.id = "menuItem1";
+    menuItem1.innerText = "MenuItem1";
+
+    const menuItem2 = document.createElement('a');
+    menuItem2.setAttribute('href', "#")
+    menuItem2.id = "menuItem2";
+    menuItem2.innerText = "MenuItem2";
+
+    const menuItem3 = document.createElement('a');
+    menuItem3.setAttribute('href', "#")
+    menuItem3.id = "menuItem3";
+    menuItem3.innerText = "MenuItem3";
+
+    dropItemsContainer.appendChild(menuItem1);
+    dropItemsContainer.appendChild(menuItem2);
+    dropItemsContainer.appendChild(menuItem3);
+
+    dropDownContainer.appendChild(dropDownButton);
+    dropDownContainer.appendChild(dropItemsContainer);
+
+    displayContainer.appendChild(dropDownContainer);
+};
+
+export default displayTaskMainMenu;
