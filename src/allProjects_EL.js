922b9ec492e4fc0697bd463ddf895f0183008147
KP <kpinc425@gmail.com>
2022-07-18 14:53:17 -0700

    Adjust dynamic label names based on state

--- src/allProjects_EL.js
+++ src/allProjects_EL.js
@@ -1,19 +1,21 @@
 import displayProjectLibrary from "./displayProjectLibrary";
+import displayProjectTitle from "./displayProjectTitle";
 import showProjectsEditButtonEventListeners from "./showProjectsEditButtons_EL";
 import showSelectedProjectTasksEL from "./showSelectedProject_EL";
 
 function allProjectsEventListener(projectArray) {
     const mainContainer = document.querySelector('main');
     // const headerContainer = document.querySelector('header');
-    const mainMenuButton = document.querySelector('.dropDownButton');
+    // const mainMenuButton = document.querySelector('.dropDownButton');
     const allProjectsButton = document.getElementById('allProjects');
 
     allProjectsButton.addEventListener('click', () => {
         mainContainer.innerHTML = "";
         // headerContainer.innerHTML = "";
+        displayProjectTitle("All Projects");
         displayProjectLibrary(projectArray, mainContainer);
         // console.log(projectArray);
-        mainMenuButton.textContent = "Projects Menu";
+        // mainMenuButton.textContent = "Projects Menu";
         // showProjectsEditButtonEventListeners();
         showSelectedProjectTasksEL(projectArray);
     })
